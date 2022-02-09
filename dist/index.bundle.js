b8cb036031064015d38ffd7c9c9ae9e7d65d9af9
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-02-09 17:55:14 +0000

    Update Logic for API Ftech and Post

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\n//# sourceURL=webpack://webpack/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\r\n\r\nfetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/')\r\n  .then(response => response.json())\r\n  .then(json => console.log(json));\r\n\n\n//# sourceURL=webpack://webpack/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import './style.css';
+let leaderboard = document.querySelector('.items');
+const FORM = document.querySelector('form');
+const NAME = FORM.querySelector('input');
+const SCORE = FORM.querySelector('input[type="number"]');
+const REFRESH = document.querySelector('#refresh');
+
+(() => {
+  FORM.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/oHFx2AoOEAVDqu2U4dC1/scores',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: NAME.value,
+          score: SCORE.value,
+        }),
+      }
+    );
+  });
+})();
+
+const refreshLeaderboard = async () => {
+  let response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hu7RDeMDKj2AivBi1yhx/scores'
+  );
+  let scoreText = await response.text();
+  let score = JSON.parse(scoreText);
+  score.result.forEach((player) => {
+    leaderboard.innerHTML += `<tr>
+      <td>${player.user}: ${player.score}</td>
+    </tr>`;
+  });
+};
+
+
+REFRESH.addEventListener('click', () => {
+  leaderboard.innerHTML = '';
+  refreshLeaderboard();
+});
