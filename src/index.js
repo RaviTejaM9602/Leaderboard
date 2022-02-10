890cacd7055f61c13df8693fe2834d984d22988a
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-02-10 16:54:52 +0000

    Add Logic for Updating the API with Input

--- src/index.js
+++ src/index.js
@@ -7,7 +7,28 @@ const NAME = FORM.querySelector('input');
 const SCORE = FORM.querySelector('input[type="number"]');
 const REFRESH = document.querySelector('#refresh');
 
-fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BxXNm40rTdIjhpN9YGnO/scores')
-  .then(response => response.json())
-  .then(json => console.log(json));
-  
+(() => {
+  FORM.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BxXNm40rTdIjhpN9YGnO/scores',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: NAME.value,
+          score: SCORE.value,
+        }),            
+      },
+      refreshInput()
+    );
+  });
+})();
+
+const refreshInput = () => {
+   NAME.value = '';
+   SCORE.value = '';
+}
+
