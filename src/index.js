4aca162306b81401fcc50aefe9d3a2420ce96e67
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-02-10 16:52:57 +0000

    Fetch Api Succesfully

--- src/index.js
+++ src/index.js
@@ -7,3 +7,7 @@ const NAME = FORM.querySelector('input');
 const SCORE = FORM.querySelector('input[type="number"]');
 const REFRESH = document.querySelector('#refresh');
 
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BxXNm40rTdIjhpN9YGnO/scores')
+  .then(response => response.json())
+  .then(json => console.log(json));
+  
